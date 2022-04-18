8175555fac079405dd51a196e250d5ffbecb46dc
Makdoom Shaikh <makshaikh99@gmail.com>
2022-04-18 18:55:30 +0530

    reconfigured authSlice

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -1,8 +1,7 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import authService from "./authService";
+import { createSlice } from "@reduxjs/toolkit";
 
 // Get user from localstorage
-const user = JSON.parse(localStorage.getItem("user"));
+const user = JSON.parse(localStorage.getItem(auth));
 
 const initialState = {
   user: user ? user : null,
@@ -12,50 +11,20 @@ const initialState = {
   message: "",
 };
 
-export const register = createAsyncThunk(
-  "auth/register",
-  async (user, thunkAPI) => {
-    try {
-      return await authService.registerUser(user);
-    } catch (error) {
-      console.log(error);
-    }
-  }
-);
-
-export const authSlice = createSlice({
+const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {
+    // reducers goes here
     reset: (state, action) => {
-      state.isLoading = false;
       state.isError = false;
       state.isSuccess = false;
+      state.isLoading = false;
       state.message = "";
     },
   },
 });
 
-export const selectUser = (state) => state.auth;
-export const { reset } = authSlice.actions;
+export const {} = authSlice.actions;
 
 export default authSlice.reducer;
-
-// // Register a user
-// export const userRegistration = createAsyncThunk(
-//   "auth/register",
-//   async (user, thunkAPI) => {
-//     // ::user will pass from register component while dispatching register
-
-//     try {
-//       return await authService.registerUser(user);
-//     } catch (error) {
-//       const message =
-//         (error.response && error.response.data && error.response.data.message) ||
-//         error.message ||
-//         error.toString();
-
-//       return thunkAPI.rejectWithValue(message);
-//     }
-//   }
-// );
