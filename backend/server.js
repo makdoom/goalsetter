0c36562fbb6c44f4d9b989b4b58905b1ed4ef40b
Makdoom Shaikh <makshaikh99@gmail.com>
2022-04-21 18:14:43 +0530

    Installed react frontend with redux toolkit

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,9 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 const colors = require("colors");
 const morgan = require("morgan");
-// const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
 const errorHandler = require("./utils/error");
+
 // Connection to DB
 connectDB();
 
@@ -17,8 +17,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(morgan("dev"));
 
 // Routes
-app.use("/api/goals", require("./routes/goalRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
+app.use("/api/goals", require("./routes/goalRoutes"));
 app.use(errorHandler);
 
 // Server lister
---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Counter } from "./features/counter/Counter";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="font-poppins">Hello iNotes</h1>
+    </div>
+  );
+}
+
+export default App;
